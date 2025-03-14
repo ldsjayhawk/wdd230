b90e064bce7936456405867d857b772138f1b223
ldsjayhawk <crisbecca@gmail.com>
2025-03-14 16:38:05 -0500

    complete bom activity - not working

---
+++ week02/scripts/bom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener("click", () => {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const delButton = document.createElement('button');
+        li.textContent = input.value;
+        delButton.textContent = '❌';
+        li.append(delButton);
+        list.appendChild(li);
+        delButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+
+    } else {
+        console.log('You did not enter a scripture')
+        input.focus();
+    }
+});
